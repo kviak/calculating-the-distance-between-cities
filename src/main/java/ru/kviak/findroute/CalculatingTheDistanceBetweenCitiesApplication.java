da6b44721912d795032327d4f8f76a76470346c2
Denis Lapin <kviakit@Gmail.com>
2023-10-19 23:25:56 +0300

    Initial commit

---
+++ src/main/java/ru/kviak/findroute/CalculatingTheDistanceBetweenCitiesApplication.java
@@ -0,0 +1,13 @@
+package ru.kviak.findroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatingTheDistanceBetweenCitiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatingTheDistanceBetweenCitiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kviak/findroute/CalculatingTheDistanceBetweenCitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kviak.findroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatingTheDistanceBetweenCitiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
